fd3fa7197d0f858cdfc1dd5503c19c0cc0db16fc
Denis Lochmelis <dlochmelis@gmail.com>
2020-06-25 21:31:49 +0300

    Fix cmake

--- hw01/bmp.h
+++ hw01/bmp.h
@@ -1,45 +1,54 @@
 #ifndef bmp_h_included
 #define bmp_h_included
 
-typedef struct
+#ifdef __cplusplus
+extern "C"
 {
-	unsigned char *header;
-	unsigned char **pixels;
-} BmpImage;
+#endif
 
-extern const unsigned int HEADER_LENGTH;
-extern const unsigned int BYTES_PER_PIXEL;
-extern const unsigned int BYTEPOS_RASTER_TOTAL_LENGTH;
-extern const unsigned int BYTEPOS_RASTER_WIDTH;
-extern const unsigned int BYTEPOS_RASTER_HEIGHT;
-extern const unsigned int BYTEPOS_FILE_SIZE;
+	typedef struct
+	{
+		unsigned char *header;
+		unsigned char **pixels;
+	} BmpImage;
 
-// RV stands for Return Value
-extern const char *RV_TYPES[];
-extern const unsigned int RV_TYPES_AMOUNT;
-extern const int RV_SUCCESS;
-extern const int RV_FILE_ERROR;
-extern const int RV_IO_ERROR;
-extern const int RV_MEMORY_ERROR;
-extern const int RV_INVALID_ARGS;
+	extern const unsigned int HEADER_LENGTH;
+	extern const unsigned int BYTES_PER_PIXEL;
+	extern const unsigned int BYTEPOS_RASTER_TOTAL_LENGTH;
+	extern const unsigned int BYTEPOS_RASTER_WIDTH;
+	extern const unsigned int BYTEPOS_RASTER_HEIGHT;
+	extern const unsigned int BYTEPOS_FILE_SIZE;
 
-unsigned int get_raster_length(const unsigned char *header);
-void set_raster_length(unsigned char *header, unsigned int length);
+	// RV stands for Return Value
+	extern const char *RV_TYPES[];
+	extern const unsigned int RV_TYPES_AMOUNT;
+	extern const int RV_SUCCESS;
+	extern const int RV_FILE_ERROR;
+	extern const int RV_IO_ERROR;
+	extern const int RV_MEMORY_ERROR;
+	extern const int RV_INVALID_ARGS;
 
-unsigned int get_raster_width(const unsigned char *header);
-void set_raster_width(unsigned char *header, unsigned int width);
+	unsigned int get_raster_length(const unsigned char *header);
+	void set_raster_length(unsigned char *header, unsigned int length);
 
-unsigned int get_raster_height(const unsigned char *header);
-void set_raster_height(unsigned char *header, unsigned int height);
+	unsigned int get_raster_width(const unsigned char *header);
+	void set_raster_width(unsigned char *header, unsigned int width);
 
-void reset_bmp_image(BmpImage *img);
-void flush_bmp_image(BmpImage *img);
+	unsigned int get_raster_height(const unsigned char *header);
+	void set_raster_height(unsigned char *header, unsigned int height);
 
-int load_bmp(const char *input_file, BmpImage *image);
+	void reset_bmp_image(BmpImage *img);
+	void flush_bmp_image(BmpImage *img);
 
-int crop(BmpImage *image, unsigned int x, unsigned int y, unsigned int w, unsigned int h);
-int rotate(BmpImage *image);
+	int load_bmp(const char *input_file, BmpImage *image);
 
-int save_bmp(BmpImage *image, const char *output_file);
+	int crop(BmpImage *image, unsigned int x, unsigned int y, unsigned int w, unsigned int h);
+	int rotate(BmpImage *image);
+
+	int save_bmp(BmpImage *image, const char *output_file);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- src/th.cpp
+++ src/th.cpp
@@ -17,7 +17,7 @@ double VAR2 = 0; // M_PI * 3.5 / 4;
 double VAR3 = 0; // M_PI * 2 / 4;
 
 double VAR4 = 1;
-double VAR5 = 1;
+double VAR5 = -1;
 double VAR6 = 1;
 
 double VAR7 = 0;
